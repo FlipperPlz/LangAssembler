ff8edd55e714207d625f0f4afe5f0269a13ac58b
ryann <flipperplz@protonmail.com>
2023-08-25 20:53:03 -0700

    feat(preprocessor): Add PreProcessor project and preprocess methods to Parser
    This commit introduces a new project, LangAssembler.PreProcessor, which contains two new parser extension methods: ProcessAndParse, each taking different sets of parameters. These new methods apply preprocessing on a lexer before parsing. The PreProcessor project was added to support initial transformation or extraction of data on a lexer before parsing.

---
+++ src/LangAssembler.PreProcessor/IPreProcessor.cs
@@ -0,0 +1,19 @@
+﻿using LangAssembler.Lexer.Base;
+using Microsoft.Extensions.Logging;
+
+namespace LangAssembler.PreProcessor;
+
+/// <summary>
+/// Defines a contract for a preprocessor that performs initial transformation or extraction of data on a lexer before parsing.
+/// </summary>
+/// <typeparam name="TLexer">The type of lexer that this preprocessor processes.</typeparam>
+public interface IPreProcessor<in TLexer> 
+    where TLexer : ILexer
+{
+    /// <summary>
+    /// Applies processing logic to the provided lexer.
+    /// </summary>
+    /// <param name="lexer">The lexer to be processed.</param>
+    /// <param name="logger">Optional logger for capturing process information.</param>
+    public void ProcessLexer(TLexer lexer, ILogger? logger);
+}
