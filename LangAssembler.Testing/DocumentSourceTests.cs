579486d64c0450d4dafac2d9a044db8902a78a51
ryann <flipperplz@protonmail.com>
2023-09-07 13:11:58 -0700

    feat(document): Add Project For Unit Testing

---
+++ LangAssembler.Testing/DocumentSourceTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using LangAssembler.Models.Doc.Source;
+using static NUnit.Framework.Assert;
+
+namespace LangAssembler.Testing;
+
+[TestFixture]
+public class DocumentSourceTests
+{
+
+    public const string TestData = "Test Doc Test Doc";
+
+    [SetUp]
+    public void Setup()
+    {
+        File.WriteAllText("Test.txt", TestData, Encoding.UTF8);
+    }
+
+    [TearDown]
+    public void Cleanup()
+    {
+        File.Delete("Test.txt");
+    }
+    
+    [Test]
+    public void TestCreate_ReadonlyFilesystem_DocumentSource()
+    {
+        var info = new FileInfo("Test.txt");
+        using var documentSource = new DocumentSource.FileSystem(info, FileMode.Open, FileAccess.Read);
+        
+        Assert.Multiple(() =>
+        {
+            That(documentSource.File, Is.EqualTo(info));
+            That(documentSource.Stream, Is.Not.Null);
+            That(documentSource.IsVirtual, Is.False);
+            That(documentSource.CanWrite, Is.False);
+            That(documentSource.CanResize, Is.False);
+        });
+    }
+}
---
+++ LangAssembler.Testing/DocumentTests.cs
@@ -0,0 +1,31 @@
+﻿using LangAssembler.Models.Doc;
+using LangAssembler.Models.Doc.Source;
+using Moq;
+
+namespace LangAssembler.Testing;
+
+[TestFixture]
+public class DocumentTests
+{
+    private Mock<DocumentSource> _source;
+    private Document _document;
+
+    
+    [SetUp]
+    public void Setup()
+    {
+        CreateDocumentSource();
+    }
+
+    private void CreateDocumentSource()
+    {
+    }
+
+
+    [TearDown]
+    public async ValueTask  Cleanup()
+    {
+        await _document.DisposeAsync();
+    }
+
+}
